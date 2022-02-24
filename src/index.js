28bdbb45799a5c8bd64f42f1bf94e74eb4958ceb
Olga Prudnik <80707801+OlgaPrudnik@users.noreply.github.com>
2022-02-24 20:24:30 +0300

    feat: update solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
   
+  n = Math.abs(n);
+
+  let array = n.toString().split('');
+
+  let revers = array.reverse();
+
+  let numbers = revers.join('');
+
+  numbers = Number(numbers);
+
+  return numbers;
+
 }
